db3d68b969d1705f6442bed1a63835027d8fc3f4
Michael Brevig <michaelbrevig@gmail.com>
2015-07-01 14:28:19 -0500

    ghetto - restricted use

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -16,10 +16,33 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'dashboard'], function(){
-    Route::get('','DashboardController@index');
+    //this is ghetto
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+//dd($ipaddress);
+    if( $ipaddress == '192.168.56.1' || $ipaddress == '173.160.114.245') {
 
-    Route::post('meta/post-update', 'DashboardController@postUpdate');
-    Route::get('meta','DashboardController@meta');
+        Route::get('', 'DashboardController@index');
+
+        Route::post('meta/post-update', 'DashboardController@postUpdate');
+        Route::get('meta', 'DashboardController@meta');
+    } else {
+        dd('hello world');
+    }
+
 
 
     //Route::get('show/{id}', 'DashboardController@show');
